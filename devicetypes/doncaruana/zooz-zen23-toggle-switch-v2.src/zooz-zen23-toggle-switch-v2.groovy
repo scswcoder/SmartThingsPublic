c047a2588b29648bb7b8b2cf5a84244b3a0d4e75
doncaruana <don@caruana.org>
2017-09-06 18:36:07 -0400

    Update zooz-zen23-toggle-switch-v2.groovy
    Match ST color scheme, remove levels in simulation

--- devicetypes/doncaruana/zooz-zen23-toggle-switch-v2.src/zooz-zen23-toggle-switch-v2.groovy
+++ devicetypes/doncaruana/zooz-zen23-toggle-switch-v2.src/zooz-zen23-toggle-switch-v2.groovy
@@ -36,19 +36,10 @@ metadata {
 	simulator {
 		status "on":  "command: 2003, payload: FF"
 		status "off": "command: 2003, payload: 00"
-		status "09%": "command: 2003, payload: 09"
-		status "10%": "command: 2003, payload: 0A"
-		status "33%": "command: 2003, payload: 21"
-		status "66%": "command: 2003, payload: 42"
-		status "99%": "command: 2003, payload: 63"
 
 		// reply messages
 		reply "2001FF,delay 5000,2602": "command: 2603, payload: FF"
 		reply "200100,delay 5000,2602": "command: 2603, payload: 00"
-		reply "200119,delay 5000,2602": "command: 2603, payload: 19"
-		reply "200132,delay 5000,2602": "command: 2603, payload: 32"
-		reply "20014B,delay 5000,2602": "command: 2603, payload: 4B"
-		reply "200163,delay 5000,2602": "command: 2603, payload: 63"
 	}
 
 	preferences {
@@ -58,9 +49,9 @@ metadata {
 	tiles(scale: 2) {
 		multiAttributeTile(name:"switch", type: "lighting", width: 6, height: 4, canChangeIcon: true){
 			tileAttribute ("device.switch", key: "PRIMARY_CONTROL") {
-				attributeState "on", label:'${name}', action:"switch.off", icon:"st.switches.light.on", backgroundColor:"#79b821", nextState:"turningOff"
+				attributeState "on", label:'${name}', action:"switch.off", icon:"st.switches.light.on", backgroundColor:"#00a0dc", nextState:"turningOff"
 				attributeState "off", label:'${name}', action:"switch.on", icon:"st.switches.light.off", backgroundColor:"#ffffff", nextState:"turningOn"
-				attributeState "turningOn", label:'${name}', action:"switch.off", icon:"st.switches.light.on", backgroundColor:"#79b821", nextState:"turningOff"
+				attributeState "turningOn", label:'${name}', action:"switch.off", icon:"st.switches.light.on", backgroundColor:"#00a0dc", nextState:"turningOff"
 				attributeState "turningOff", label:'${name}', action:"switch.on", icon:"st.switches.light.off", backgroundColor:"#ffffff", nextState:"turningOn"
 			}
 		}
@@ -86,7 +77,7 @@ private getCommandClassVersions() {
 		0x5E: 2,  // ZwaveplusInfo
 		0x27: 1,  // All Switch
 		0x25: 1,  // Binary Switch
-    0x70: 1,  // Configuration
+		0x70: 1,  // Configuration
 		0x20: 1,  // Basic
 	]
 }
