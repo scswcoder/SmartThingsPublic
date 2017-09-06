fe4b2fd78f437eb11574fbf6ef2235913ad6394f
doncaruana <don@caruana.org>
2017-09-06 17:24:45 -0400

    Create zen22.groovy

---
+++ devicetypes/doncaruana/zen22.src/zen22.groovy
@@ -0,0 +1,11 @@
+/**
+ *  Trying to get something to sync
+ */
+
+metadata {
+	definition (name: "testing", namespace: "doncaruana", author: "Don Caruana") {
+		capability "Switch"
+		capability "Refresh"
+	}
+}
+//this code has no newline at the end.
