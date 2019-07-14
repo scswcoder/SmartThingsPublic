686c8ad6c9dad7441b22711092dafe672542343e
doncaruana <don@caruana.org>
2019-07-13 23:22:23 -0400

    fix null preference logic

--- devicetypes/doncaruana/zooz-zen23-toggle-switch-v3.src/zooz-zen23-toggle-switch-v3.groovy
+++ devicetypes/doncaruana/zooz-zen23-toggle-switch-v3.src/zooz-zen23-toggle-switch-v3.groovy
@@ -142,7 +142,8 @@ def installed() {
 def updated(){
 	log.debug "updated()"
 	// These are needed as SmartThings is not honoring defaultValue in preferences. They are set to the device defaults
-	def setLocalControl = localControl == null ? 1 : localControl == true ? 1 : 0
+	def setLocalControl = 1
+	if (localControl!=null) {setLocalControl = localControl == true ? 1 : 0}
 	def setOffTimer = 60
 	if (offTimer) {setOffTimer = offTimer}
 	def setOnTimer = 60
@@ -158,7 +159,7 @@ def updated(){
 	}
 	def setScene = sceneCtrl == true ? 1 : 0
 	def setPowerRestore = 2
-	if (powerRestore) {setPowerRestore = powerRestore == "prremember" ? 2 : powerRestore == "proff" ? 0 : 1}
+	if (powerRestore != null) {setPowerRestore = powerRestore == "prremember" ? 2 : powerRestore == "proff" ? 0 : 1}
 	def setAutoTurnon = autoTurnon == true ? 1 : 0
 	def setAutoTurnoff = autoTurnoff == true ? 1 : 0
 	def setToggleControl = 0
