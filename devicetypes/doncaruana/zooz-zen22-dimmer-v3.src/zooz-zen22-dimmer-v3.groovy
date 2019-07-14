093f50516f9f79314edb2c596a33b94fd63ffabd
doncaruana <don@caruana.org>
2019-07-13 23:23:50 -0400

    fix null preference logic

--- devicetypes/doncaruana/zooz-zen22-dimmer-v3.src/zooz-zen22-dimmer-v3.groovy
+++ devicetypes/doncaruana/zooz-zen22-dimmer-v3.src/zooz-zen22-dimmer-v3.groovy
@@ -171,7 +171,7 @@ def installed() {
 def updated(){
 	// These are needed as SmartThings is not honoring defaultValue in preferences. They are set to the device defaults
 	def setLocalControl = 1
-	if (localControl) {setLocalControl = localControl == true ? 1 : 0}
+	if (localControl!=null) {setLocalControl = localControl == true ? 1 : 0}
 	def setOffTimer = 60
 	if (offTimer) {setOffTimer = offTimer}
 	def setOnTimer = 60
@@ -194,7 +194,7 @@ def updated(){
 	def setScene = sceneCtrl == true ? 1 : 0
 	def setDoubleTap = doubleTap == true ? 1 : 0
 	def setPowerRestore = 2
-	if (powerRestore) {setPowerRestore = powerRestore == "prremember" ? 2 : powerRestore == "proff" ? 0 : 1}
+	if (powerRestore != null) {setPowerRestore = powerRestore == "prremember" ? 2 : powerRestore == "proff" ? 0 : 1}
 	def setAutoTurnon = autoTurnon == true ? 1 : 0
 	def setAutoTurnoff = autoTurnoff == true ? 1 : 0
 	def setPaddleControl = 0
